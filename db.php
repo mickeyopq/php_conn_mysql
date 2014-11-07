2964b081b1a793b6c81190374c4d32c5ada8d0b9
mickeyopq <mickeyopq@gmail.com>
2014-11-07 17:05:14 +0800

    fdfsde34r34r

--- db.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$link = mysql_connect('127.0.0.1','root','123');
-	if (!$link){
-		die('Could not connect:' . mysql_error());
-	}
-	echo "Database connected successfully\n";
-	mysql_close($link);
-?>
